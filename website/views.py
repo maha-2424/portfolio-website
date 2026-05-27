ab6b4407dad82c71526fd9b31eb7a33b7550a85a
maha-2424 <mahareddy34@gmail.com>
2026-05-27 12:10:13 +0530

    Initial portfolio project

---
+++ website/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import Contact
+
+def home(request):
+
+    if request.method == 'POST':
+
+        name = request.POST.get('name')
+
+        email = request.POST.get('email')
+
+        message = request.POST.get('message')
+
+        Contact.objects.create(
+            name=name,
+            email=email,
+            message=message
+        )
+
+    return render(request, 'home.html')
